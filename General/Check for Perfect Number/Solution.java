e4647f4a78253682cff93ec64171c02b056641ba
sumit573 <52561251+sumit573@users.noreply.github.com>
2026-07-01 23:09:57 +0530

    Solved: Check for Perfect Number [Unknown] — Java

---
+++ General/Check for Perfect Number/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPerfect(int n) {
+        int sum=0;
+        for(int i=1; i<n;i++){
+            if(n%i==0){
+                sum = sum +i;
+            }
+        }
+
+        if(sum==n) return true;
+        return false;
+
+    }
+}
