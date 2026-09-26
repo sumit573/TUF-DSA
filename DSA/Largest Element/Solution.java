6cc10c694383384760719e6f9c7d6338e3275dd7
sumit573 <52561251+sumit573@users.noreply.github.com>
2026-09-26 12:20:02 +0530

    Solved Largest Element [Basic] — Java

--- DSA/Largest Element/Solution.java
+++ DSA/Largest Element/Solution.java
@@ -1,12 +1,11 @@
 class Solution {
     public int largestElement(int[] nums) {
-    return largeEle;
-         int largeEle = nums[0];
-        for(int i=1;i<nums.length;i++){
-        }
+        int largeEle = nums[0];
+        for(int i=0;i<nums.length;i++){
             if(nums[i] > largeEle){
-               largeEle = nums[i];
+        }
+                largeEle = nums[i];
             }
     }
-
+        return largeEle;
 }
